d1689049f71470da1ee8500d527d595ab4c072ff
massinissamohellebi <massinissa.mohellebi@gmail.com>
2019-02-19 17:16:20 +0100

    add groovyfile

--- dsl.groovy
+++ dsl.groovy
@@ -1,4 +1,9 @@
+def gitUrl = 'https://github.com/massinissamohellebi/JenkinsTpNode.git'
+
 job('test-job') {
+    scm {
+        git(gitUrl)
+    }
     triggers {
         scm('*/5 * * * *')
     }
